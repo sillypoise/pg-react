ffec6d65963c0a6c7c3564604fb9a54c85602ef0
sillypoise <somewhatjose@gmail.com>
2022-10-17 15:06:01 -0500

    project/react-training: Added first exercise

--- projects/react-training/src/App.tsx
+++ projects/react-training/src/App.tsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { TabList, TabPanel, Tabs, Tab, TabPanels } from "@reach/tabs";
+import { HTMLAttributes, useState } from "react";
 
 function App() {
     const [count, setCount] = useState(0);
@@ -8,6 +9,25 @@ function App() {
             <article className="stack">
                 <h2>React-Training ground</h2>
                 <p>Let's get this started!</p>
+                <hr />
+                <Tabs className="stack">
+                    <TabList>
+                        <Tab className="pli-s plb-2xs text-[color:var(--neutral-on-surface-1)]">
+                            Login
+                        </Tab>
+                        <Tab className="pli-s plb-2xs text-[color:var(--neutral-on-surface-1)]">
+                            Signup
+                        </Tab>
+                    </TabList>
+                    <TabPanels>
+                        <TabPanel>
+                            <p>Login form</p>
+                        </TabPanel>
+                        <TabPanel>
+                            <p>Signup form</p>
+                        </TabPanel>
+                    </TabPanels>
+                </Tabs>
             </article>
         </main>
     );
