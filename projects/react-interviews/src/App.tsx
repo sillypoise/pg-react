e27310e17383211ae2238f27aaac3f70f18bac18
sillypoise <somewhatjose@gmail.com>
2022-10-18 15:28:35 -0500

    p/react-interviews-super-market-0: Coded up the whole solution

--- projects/react-interviews/src/App.tsx
+++ projects/react-interviews/src/App.tsx
@@ -1,10 +1,58 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import { api } from "./api";
+import { Item } from "./types";
 
 function App() {
+    let [items, setItems] = useState<Array<Item> | null>(null);
+    let inputItemRef = useRef<HTMLInputElement>(null);
+    useEffect(() => {
+        inputItemRef.current?.focus();
+    }, []);
+    useEffect(() => {
+        api.list().then((data) => setItems(data));
+    }, []);
+    function handleItemDelete(id: number) {
+        let newItems = items && items.filter((item) => item.id !== id);
+        setItems(newItems);
+    }
+
     return (
-        <main className="stack center mlb-l">
-            <article>
-                <h1 className="text-3">React Interview Challenges</h1>
+        <main>
+            <article className="stack center mlb-l">
+                <h1 className="text-3">Super Market Challenge 0</h1>
+                <form
+                    action=""
+                    onSubmit={(e) => e.preventDefault()}
+                    className="stack center"
+                >
+                    <label htmlFor="list">List Items:</label>
+                    <input
+                        ref={inputItemRef}
+                        type="text"
+                        name="text"
+                        id="list"
+                    />
+                    <button>Add +</button>
+                    <ul>
+                        {!items ? (
+                            <p>Loading your data..</p>
+                        ) : (
+                            items.map((item) => (
+                                <li key={item.id} className="cluster">
+                                    <span>{item.text}</span>
+                                    <button
+                                        onClick={() =>
+                                            handleItemDelete(item.id)
+                                        }
+                                    >
+                                        [X]
+                                    </button>
+                                </li>
+                            ))
+                        )}
+                    </ul>
+                </form>
+                <pre>{JSON.stringify(items, null, 4)}</pre>
             </article>
         </main>
     );
