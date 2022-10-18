a8282cb3825e28d08baaa54b26437744770a0959
sillypoise <somewhatjose@gmail.com>
2022-10-17 19:06:53 -0500

    project/react-training: finished useState exercise

--- projects/react-training/src/App.tsx
+++ projects/react-training/src/App.tsx
@@ -1,8 +1,22 @@
 import { TabList, TabPanel, Tabs, Tab, TabPanels } from "@reach/tabs";
+import { VisuallyHidden } from "@reach/visually-hidden";
 import { HTMLAttributes, useState } from "react";
 
 function App() {
-    const [count, setCount] = useState(0);
+    let [minutes, setMinutes] = useState(1);
+    let [error, setError] = useState<String | null>(null);
+    let [showPassword, setShowPassword] = useState(false);
+    function handleSub() {
+        if (minutes > 1) {
+            setMinutes(minutes - 1);
+        } else {
+            setError("Can't work in the negative zone bud");
+        }
+    }
+    function handleAdd() {
+        setMinutes(minutes + 1);
+        setError(null);
+    }
 
     return (
         <main className="center [--center-width:theme(contentWidth.3)] mlb-l">
@@ -10,24 +24,49 @@ function App() {
                 <h2>React-Training ground</h2>
                 <p>Let's get this started!</p>
                 <hr />
-                <Tabs className="stack">
-                    <TabList>
-                        <Tab className="pli-s plb-2xs text-[color:var(--neutral-on-surface-1)]">
-                            Login
-                        </Tab>
-                        <Tab className="pli-s plb-2xs text-[color:var(--neutral-on-surface-1)]">
-                            Signup
-                        </Tab>
-                    </TabList>
-                    <TabPanels>
-                        <TabPanel>
-                            <p>Login form</p>
-                        </TabPanel>
-                        <TabPanel>
-                            <p>Signup form</p>
-                        </TabPanel>
-                    </TabPanels>
-                </Tabs>
+                {/* <VisuallyHidden> */}
+
+                <form action="">
+                    <fieldset className="">
+                        <legend>Fill out your login information</legend>
+                        <div className="stack [--stack-gap:theme(spacing.xs)]">
+                            <label htmlFor="login:email">Email:</label>
+                            <input
+                                type="text"
+                                name=""
+                                id="login:email"
+                                className="outline rounded-md outline-offset-2 outline-[color:var(--neutral-border-0)] focus:bg-light-amber-7 p-3xs"
+                                placeholder="you@example.com"
+                            />
+                            <label htmlFor="login:password">Password:</label>
+                            <input
+                                type={showPassword ? "text" : "password"}
+                                name=""
+                                id="login:password"
+                                className="outline rounded-md outline-offset-2 outline-[color:var(--neutral-border-0)] focus:bg-light-amber-7 p-3xs"
+                                placeholder="Password"
+                            />
+                            <div className="cluster gap-xs">
+                                <input
+                                    type="checkbox"
+                                    name=""
+                                    id="login:toggle-password-visibilty"
+                                    defaultChecked={showPassword}
+                                    onChange={() =>
+                                        setShowPassword((val) => !val)
+                                    }
+                                />
+                                <label
+                                    className="text-00"
+                                    htmlFor="login:toggle-password-visibilty"
+                                >
+                                    show password
+                                </label>
+                            </div>
+                        </div>
+                    </fieldset>
+                </form>
+                {/* </VisuallyHidden> */}
             </article>
         </main>
     );
