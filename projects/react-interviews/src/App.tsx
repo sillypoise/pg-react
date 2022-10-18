2808ce494afc2b9c39c7542f800798b974bacb09
sillypoise <somewhatjose@gmail.com>
2022-10-18 14:55:22 -0500

    project/react-interviews: Wrote initial project setup + tailwind styles

---
+++ projects/react-interviews/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+    return (
+        <main className="stack center mlb-l">
+            <article>
+                <h1 className="text-3">React Interview Challenges</h1>
+            </article>
+        </main>
+    );
+}
+
+export default App;
