bb87fe03d85466edc8c43f7c1595444032316caa
sillypoise <somewhatjose@gmail.com>
2022-10-20 12:29:03 -0500

    p/react-memo: mocked api data and rendered list of data

--- projects/react-memo/src/App.tsx
+++ projects/react-memo/src/App.tsx
@@ -1,13 +1,32 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { api } from "./api";
 
 function App() {
-    const [count, setCount] = useState(0);
+    const [people, setPeople] = useState<
+        Array<{ id: number; name: string; age: number }>
+    >([]);
+
+    useEffect(() => {
+        api.list().then((data) => setPeople(data));
+    }, [people]);
 
     return (
         <main className="mlb-l">
             <article className="center stack">
                 <h1 className="text-3">React memo</h1>
                 <p>What is it good for?</p>
+                <hr />
+                <ul role="list" className="auto-grid">
+                    {people.map((person) => (
+                        <li
+                            key={person.id}
+                            className="box p-s cluster justify-between rounded-md"
+                        >
+                            <p className="font-semibold">{person.name}</p>
+                            <p>{person.age}</p>
+                        </li>
+                    ))}
+                </ul>
             </article>
         </main>
     );
