b36b3f27cf0947ae00214cd16648b9c653699e8e
sillypoise <somewhatjose@gmail.com>
2022-10-20 14:48:15 -0500

    p/react-useMemo: Working along on the example for useMemo

--- projects/react-useMemo/src/App.tsx
+++ projects/react-useMemo/src/App.tsx
@@ -1,40 +1,5 @@
-import React, { memo, useEffect, useState } from "react";
+import React, { ChangeEvent, memo, useEffect, useState } from "react";
 import { api } from "./api";
-
-let RandomPerson = memo(function RandomPerson() {
-    const [people, setPeople] = useState<
-        Array<{ id: number; name: string; age: number }>
-    >([]);
-
-    useEffect(() => {
-        api.list().then((data) => setPeople(data));
-    }, []);
-
-    return (
-        <article className="stack [--stack-gap:theme(spacing.xs)] mbs-m | debug">
-            <p>
-                This component won't re-render every time <code>App</code>{" "}
-                re-renders because it has been memoized. We control the
-                rendering of this component now thanks to{" "}
-                <code>React.memo</code>
-            </p>
-            <ul role="list" className="auto-grid">
-                {[...people]
-                    .sort(() => (Math.random() > 0.5 ? 1 : -1))
-                    .slice(0, 2)
-                    .map((person) => (
-                        <li
-                            key={person.id}
-                            className="box p-s cluster justify-between rounded-md"
-                        >
-                            <p className="font-semibold">{person.name}</p>
-                            <p>{person.age}</p>
-                        </li>
-                    ))}
-            </ul>
-        </article>
-    );
-});
 
 function App() {
     let [people, setPeople] = useState<
@@ -42,6 +7,7 @@ function App() {
     >([]);
     let [query, setQuery] = useState("");
     let [renderCount, setRenderCount] = useState(0);
+    let [sortOption, setSortOption] = useState("name");
 
     useEffect(() => {
         api.list().then((data) => {
@@ -53,14 +19,44 @@ function App() {
         setRenderCount((count) => count + 1);
     }, [query]);
 
+    function sortData(
+        people: Array<{ id: number; name: string; age: number }>
+    ) {
+        console.count("Running sortData! 🏃");
+        console.log("sorting by" + ` ${sortOption}`);
+        if (sortOption === "name") {
+            let sortedByName = [...people].sort((a, b) =>
+                new Intl.Collator("es").compare(a.name, b.name)
+            );
+            setPeople(sortedByName);
+        } else if (sortOption === "age") {
+            let sortedByName = [...people].sort((a, b) =>
+                a.age < b.age ? -1 : 1
+            );
+            setPeople(sortedByName);
+        } else {
+            return;
+        }
+    }
+
+    function handleSortSelect(e: ChangeEvent<HTMLSelectElement>) {
+        setSortOption(e.target.value);
+        sortData(people);
+    }
+
     return (
         <main className="mlb-l">
             <article className="center [--center-width:theme(contentWidth.3)] stack">
                 <h1 className="text-3">React useMemo</h1>
                 <p>What is it good for?</p>
                 <p>
-                    Prevents a component from re-rending when its props are
-                    unchanged, even if the parent component re-renders
+                    Lets you cache/memoize the <strong>result</strong> of a
+                    calculation between re-renders.
+                </p>
+                <p>
+                    For example, here we will pretend that sorting our array
+                    alphabetically is an expensive operation that we don't want
+                    to re-do on every re-render
                 </p>
                 <input
                     type="text"
@@ -73,6 +69,19 @@ function App() {
                 <p>{JSON.stringify(console.count("rendering"))}</p>
                 <p>Render count: {renderCount}</p>
                 <hr />
+                <form action="" className="cluster">
+                    <label htmlFor="sort-by">Sort-by</label>
+                    <select
+                        name=""
+                        id="sort-by"
+                        className="ring-4"
+                        value={sortOption}
+                        onChange={handleSortSelect}
+                    >
+                        <option value="name">name</option>
+                        <option value="age">age</option>
+                    </select>
+                </form>
                 <ul role="list" className="auto-grid">
                     {people.map((person) => (
                         <li
@@ -84,8 +93,6 @@ function App() {
                         </li>
                     ))}
                 </ul>
-                {/* <MemoizedComp /> */}
-                <RandomPerson />
             </article>
         </main>
     );
