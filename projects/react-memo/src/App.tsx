eac356b520da0062b5cd9a03c2d0de561222205d
sillypoise <somewhatjose@gmail.com>
2022-10-20 12:18:25 -0500

    p/react-memo: project setup

---
+++ projects/react-memo/src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+function App() {
+    const [count, setCount] = useState(0);
+
+    return (
+        <main>
+            <article>
+                <h1>React memo</h1>
+            </article>
+        </main>
+    );
+}
+
+export default App;
