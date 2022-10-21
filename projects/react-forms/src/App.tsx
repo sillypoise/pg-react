fb78db6b1b7673417dc406bf95de2518910c087b
sillypoise <somewhatjose@gmail.com>
2022-10-21 09:58:01 -0500

    p/react-forms: Added `select` tag exapmle to react forms

--- projects/react-forms/src/App.tsx
+++ projects/react-forms/src/App.tsx
@@ -1,12 +1,43 @@
 import { useState } from "react";
 
 function App() {
-    const [count, setCount] = useState(0);
+    const [selectValue, setSelectValue] = useState("lime");
 
     return (
         <main className="mlb-l">
             <article className="stack center">
                 <h2>React Forms</h2>
+                <h4>The select tag</h4>
+                <p>
+                    We control the select value option through the the{" "}
+                    <code>value</code> and <code>onChange</code> attributes in
+                    the select tag
+                </p>
+                <p>The selected tag is {selectValue}</p>
+                <select
+                    value={selectValue}
+                    onChange={(e) => setSelectValue(e.target.value)}
+                    className="ring-4"
+                >
+                    <option value="grapefruit">Grapefruit</option>
+                    <option value="lime">Lime</option>
+                    <option value="coconut">Coconut</option>
+                    <option value="mango">Mango</option>
+                </select>
+                <p>
+                    We can pass an array into the <code>value</code> attribute
+                    which allows us to select multiple options in a select tag
+                </p>
+                <select
+                    multiple={true}
+                    value={["lime", "coconut"]}
+                    className="ring-4"
+                >
+                    <option value="grapefruit">Grapefruit</option>
+                    <option value="lime">Lime</option>
+                    <option value="coconut">Coconut</option>
+                    <option value="mango">Mango</option>
+                </select>
             </article>
         </main>
     );
