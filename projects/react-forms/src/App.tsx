b1312f2b8f2c06a2212ae4e26f0ddfb4e1c6e9ad
sillypoise <somewhatjose@gmail.com>
2022-10-21 09:50:48 -0500

    p/react-forms: scaffolded project

---
+++ projects/react-forms/src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+function App() {
+    const [count, setCount] = useState(0);
+
+    return (
+        <main className="mlb-l">
+            <article className="stack center">
+                <h2>React Forms</h2>
+            </article>
+        </main>
+    );
+}
+
+export default App;
