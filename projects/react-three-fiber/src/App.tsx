3a22b5724b8791c10af7eb0d5b309102b1ec0b59
sillypoise <somewhatjose@gmail.com>
2022-11-04 16:01:03 -0500

    p/react-three-fiber: init commit and scaffold

---
+++ projects/react-three-fiber/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+    return (
+        <main className="mlb-l">
+            <article className="center stack">
+                <h3>React Three Fiber</h3>
+            </article>
+        </main>
+    );
+}
+
+export default App;
