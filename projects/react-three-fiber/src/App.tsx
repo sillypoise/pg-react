7d6473b22c02ccc06e1db9be718421466e5796ec
sillypoise <somewhatjose@gmail.com>
2022-11-04 17:12:51 -0500

    p/react-three-fiber: added viewport fix so every box can be calculate to render inside its own current viewport

--- projects/react-three-fiber/src/App.tsx
+++ projects/react-three-fiber/src/App.tsx
@@ -2,18 +2,19 @@ import * as THREE from "three";
 import { Canvas, useFrame, useThree } from "@react-three/fiber";
 import { useRef, useState } from "react";
 
-function Box() {
+function Box({ z }: { z: number }) {
     let ref = useRef<any>();
-    let { viewport } = useThree();
+    let { viewport, camera } = useThree();
+    let { width, height } = viewport.getCurrentViewport(camera, [0, 0, z]);
     let [clicked, setClicked] = useState(false);
     let [data] = useState({
         x: THREE.MathUtils.randFloatSpread(2),
         y: THREE.MathUtils.randFloatSpread(viewport.height),
     });
     useFrame((state) => {
-        ref.current.position.set(data.x * viewport.width, (data.y += 0.1), 0);
-        if (data.y > viewport.height / 1.5) {
-            data.y = -viewport.height / 1.5;
+        ref.current.position.set(data.x * width, (data.y += 0.25), z);
+        if (data.y > height / 1.5) {
+            data.y = -height / 1.5;
         }
     });
     return (
@@ -28,7 +29,7 @@ function App({ count = 100 }) {
     return (
         <Canvas>
             {Array.from({ length: count }, (_, i) => (
-                <Box key={i} />
+                <Box key={i} z={-i} />
             ))}
         </Canvas>
     );
