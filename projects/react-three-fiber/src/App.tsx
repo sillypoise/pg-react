17b319a622f142d4fb591a813c508861f1dcd155
sillypoise <somewhatjose@gmail.com>
2022-11-04 17:06:34 -0500

    p/react-three-fiber: Three Fiber is supah easy if you know react and abstracts away all the annoying parts of Three.js! Now it's just a matter of learning how to couple HTML to it, keep it accessible and animating it.

--- projects/react-three-fiber/src/App.tsx
+++ projects/react-three-fiber/src/App.tsx
@@ -1,10 +1,36 @@
-function App() {
+import * as THREE from "three";
+import { Canvas, useFrame, useThree } from "@react-three/fiber";
+import { useRef, useState } from "react";
+
+function Box() {
+    let ref = useRef<any>();
+    let { viewport } = useThree();
+    let [clicked, setClicked] = useState(false);
+    let [data] = useState({
+        x: THREE.MathUtils.randFloatSpread(2),
+        y: THREE.MathUtils.randFloatSpread(viewport.height),
+    });
+    useFrame((state) => {
+        ref.current.position.set(data.x * viewport.width, (data.y += 0.1), 0);
+        if (data.y > viewport.height / 1.5) {
+            data.y = -viewport.height / 1.5;
+        }
+    });
     return (
-        <main className="mlb-l">
-            <article className="center stack">
-                <h3>React Three Fiber</h3>
-            </article>
-        </main>
+        <mesh ref={ref} onClick={() => setClicked(!clicked)}>
+            <boxGeometry />
+            <meshBasicMaterial color="orange" />{" "}
+        </mesh>
+    );
+}
+
+function App({ count = 100 }) {
+    return (
+        <Canvas>
+            {Array.from({ length: count }, (_, i) => (
+                <Box key={i} />
+            ))}
+        </Canvas>
     );
 }
 
--- projects/react-three-fiber/src/main.tsx
+++ projects/react-three-fiber/src/main.tsx
@@ -5,6 +5,8 @@ import "../styles/tailwind.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
-        <App />
+        <main className="w-screen h-screen">
+            <App />
+        </main>
     </React.StrictMode>
 );
